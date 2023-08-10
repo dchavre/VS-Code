36be703a9df0337fbe6c1808877b98cd04ad8dac
Darsh Chavre <dchavre10@gmail.com>
2023-08-09 22:52:50 -0400

    Neural Network etc.

--- Python/Machine Learning FCC/Hidden Markov Model.py
+++ Python/Machine Learning FCC/Hidden Markov Model.py
@@ -1,6 +1,20 @@
 import tensorflow_probability as tfp
 import tensorflow as tf
 
-tfp = tfp.distributions
-initial_distribution = tfp.Categorical{probs}
+tfd = tfp.distributions
+initial_distribution = tfd.Categorical(probs=[0.8, 0.2]) # Intial distribution is 80%, 20%
+transition_distribution = tfd.Categorical(probs=[[0.7, 0.3], # Probabilites of a transition occuring
+                                          [0.2, 0.8]])
+observation_distribution = tfd.Normal(loc=[0., 15.], scale=[5., 10.]) # loc = mean and scale = standard deviation
 
+
+model = tfd.HiddenMarkovModel(
+    initial_distribution=initial_distribution,
+    transition_distribution=transition_distribution,
+    observation_distribution = observation_distribution,
+    num_steps=7)
+
+mean = model.mean() # Calculates probability from the model
+
+with tf.compat.v1.Session() as sess:
+    print(mean.numpy()) # Prints expected temperatures for each day
---
+++ Python/Machine Learning FCC/Neural Network I.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+from tensorflow import keras
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+fashion_mnist = keras.datasets.fashion_mnist # Loads dataset
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data() # Split into testing and training data
+
+# 28px by 28px grayscale images
+# 0-255, 0 being white, 255 being black
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# Data Preprocessing:
+train_images = train_images / 255.0 
+test_images = test_images / 255.0
+
+# Making the model:
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)), # input layer (1st); Flatten takes the 28 by 28 shape and flatten it down to 784 pixels
+    keras.layers.Dense(128, activation='relu'), # hidden layer (2nd); a dense layer, with 128 nodes (usually you want a little bit less than the nodes in the input layer), and relu is the activation function
+    keras.layers.Dense(10, activation='softmax') # output layer (3rd); a dense layer, with 10 nodes, and softmax is the activation function
+])
+
+# Compiling the model - Picking the optimizer, loss function, and the metrics
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Fitting (training) model to the training data:
+model.fit(train_images, train_labels, epochs = 10)
+
+# Evaluating the model:
+test_loss, test_acc = model.evaluate(test_images, test_labels, verbose = 1)
+print('Test accuracy:', test_acc)
